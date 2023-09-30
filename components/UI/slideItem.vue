d68742a4247b9ce6df835bede3a21e4b5c4fb389
yemref07 <yemref0716@hotmail.com>
2023-10-01 02:21:58 +0300

    add bg images and enhane responsivity

--- components/UI/slideItem.vue
+++ components/UI/slideItem.vue
@@ -1,13 +1,13 @@
 <template>
-     <div class="w-full" :style="{backgroundColor:backgroundColor}">
-        <div class="container mx-auto px-10 lg:px-20 xl:px-40" >
+     <div class="w-full relative" :style="{backgroundColor:backgroundColor}">
+        <div class="container mx-auto px-10 lg:px-20 xl:px-40">
           <div class="slide flex flex-col justify-center items-center">
             <div class="grid lg:grid-cols-2">
   
               <div class="grid-item flex flex-col justify-center">
   
                 <div class="">
-                  <h1 class="text-white font-semibold text-6xl">
+                  <h1 class="text-white font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
                     {{props.title1}}
                   </h1>
                 </div>
@@ -36,16 +36,19 @@
   
               </div>
   
-              <div class="grid-item flex justify-end items-center">
+              <div class="grid-item flex justify-start mt-12 lg:mt-0 lg:justify-end items-center">
                 <img
                   :src="props.img"
                   alt=""
-                  class="w-2/3"
+                  class="w-3/4 z-20"
                 />
               </div>
             </div>
           </div>
         </div>
+        <img src="/shape/slider-shape-2.png" alt="" class="w-40 absolute  opacity-40 top-1/4 left-20">
+        <img src="/shape/slider-shape-1.png" alt="" class="w-2/5 absolute grayscale opacity-20 top-0 right-0">
+        <img src="/shape/slider-shape-4.png" alt="" class="w-2/5 absolute grayscale opacity-20 top-0 right-28">
       </div>
 </template>
 
