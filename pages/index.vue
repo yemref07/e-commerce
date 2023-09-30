4a8ff96310370d3ea48eb5eef647bc963bdc4b8e
yemref07 <yemref0716@hotmail.com>
2023-09-30 04:35:03 +0300

    add main slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,61 @@
 <template>
-    <div>
-        Home Page
-    </div>
+  <swiper-container
+    slides-per-view="1"
+    speed="500"
+    loop="true"
+    css-mode="true"
+    pagination="true"
+    :autoplay="{
+      delay: 2500,
+      disableOnInteraction: false,
+    }"
+    :navigation="{
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }"
+  >
+    <swiper-slide>
+      <slideItem
+        title1="The Best Electronic Devices"
+        title2="Exclusive offer %10 off this week"
+        catLink="/"
+        bgColor="#115061"
+        img="/slider/slider-img-1.png"
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <slideItem
+        title1="The Best Electronic Devices"
+        title2="Exclusive offer %10 off this week"
+        catLink="/"
+        bgColor="#115061"
+        img="/slider/slider-img-2.png"
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <slideItem
+        title1="The Best Electronic Devices"
+        title2="Exclusive offer %10 off this week"
+        catLink="/"
+        bgColor="#115061"
+        img="/slider/slider-img-3.png"
+    />
+  </swiper-slide>
+  </swiper-container>
 </template>
 
 <script setup>
+import slideItem from "~/components/UI/slideItem.vue";
+import { register } from "swiper/element/bundle";
+register();
 
+const swiper = ref(null);
 </script>
 
-<style  scoped>
-
+<style scoped>
+swiper-container::part(bullet-active) {
+  background-color: white;
+}
 </style>
