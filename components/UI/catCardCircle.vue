85ed1afcd47210b78821569f401dcb67e363a8ac
yemref07 <yemref0716@hotmail.com>
2023-10-01 03:49:52 +0300

    add category card circle style compenent

---
+++ components/UI/catCardCircle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-col justify-center items-center gap-5">
+
+        <div class="flex flex-col justify-center items-center cat-circle rounded-full">
+            <nuxt-link :to="link" class="cursor-pointer">
+                <img :src="img" alt="Product Category 1" width="78">
+            </nuxt-link>
+        </div>
+
+        <div class="text-center">
+            <nuxt-link :to="link" class="cursor-pointer">
+                <h3 class="font-semibold text-xl">{{ title }}</h3>
+                <p>{{ productStock }} Products</p>
+            </nuxt-link>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title:String,
+    productStock:Number,
+    img:String,
+    link:String
+})
+
+</script>
+
+<style  scoped>
+.cat-circle{
+    background-color: #e9f4ff;
+    height: 180px;
+    width: 180px;
+}
+
+</style>
