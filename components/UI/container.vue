f0565c5adeb2a8405ce1dbb98ac59158a0e11bfa
yemref07 <yemref0716@hotmail.com>
2023-10-04 02:43:10 +0300

    add style to container compenent

--- components/UI/container.vue
+++ components/UI/container.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="container mx-auto px-10 lg:px-20 xl:px-32">
+        <div class="container mx-auto px-5 lg:px-20 xl:px-32">
             <slot></slot>
         </div>
 </template>
