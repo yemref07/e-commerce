7f127175adcc07b95e33b4124868243fcf57e169
yemref07 <yemref0716@hotmail.com>
2023-10-07 03:55:04 +0300

    add product grid display compenet

---
+++ components/productGrid.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+
+        <div class="flex flex-col">
+            <div class="mb-5">
+                <h2 class="text-xl font-semibold">Discount Products</h2>
+            </div>
+            <productGridItem />
+        </div>
+
+        <div class="flex flex-col">
+            <div class="mb-5">
+                <h2 class="text-xl font-semibold">Discount Products</h2>
+            </div>
+            <productGridItem />
+        </div>
+        
+        <div class="flex flex-col">
+            <div class="mb-5">
+                <h2 class="text-xl font-semibold">Discount Products</h2>
+            </div>
+            <productGridItem />
+        </div>
+
+
+
+    </div>
+</template>
+
+<script setup>
+import productGridItem from './UI/productGridItem.vue';
+
+</script>
+
+<style  scoped>
+
+</style>
