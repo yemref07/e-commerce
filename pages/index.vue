fd663c2085bde1a2e91c2e2a4ab8e91ec4f9c3a4
yemref07 <yemref0716@hotmail.com>
2023-10-10 03:52:54 +0300

    add trendind product comp - api fetch - tab - menu

--- pages/index.vue
+++ pages/index.vue
@@ -109,87 +109,7 @@
     </div>
   </container>
 
-  <container class="mt-20">
-    <div class="flex flex-row justify-between items-center">
-      <div class="relative">
-        <h2 class="font-semibold text-xl lg:text-4xl z-20 relative">
-          Trending Product
-        </h2>
-        <svg
-          class="text-torange absolute top-5 left-2 z-10"
-          width="120"
-          height="35"
-          viewBox="0 0 114 35"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M112 23.275C1.84952 -10.6834 -7.36586 1.48086 7.50443 32.9053"
-            stroke="currentColor"
-            stroke-width="4"
-            stroke-miterlimit="3.8637"
-            stroke-linecap="round"
-          ></path>
-        </svg>
-      </div>
-
-      <div class="grow">
-        <hr class="w-2/3 mx-auto" />
-      </div>
-
-      <div class="flex gap-5">
-        <div class="">
-          <button
-            class="text-black text-base lg:text-xl"
-            @click="
-              () => {
-                setActiveTab(1);
-              }
-            "
-          >
-            New
-          </button>
-        </div>
-
-        <div class="">
-          <button
-            class="text-muted text-base lg:text-xl"
-            @click="
-              () => {
-                setActiveTab(2);
-              }
-            "
-          >
-            Featured
-          </button>
-        </div>
-
-        <div class="">
-          <button
-            class="text-muted text-base lg:text-xl"
-            @click="
-              () => {
-                setActiveTab(3);
-              }
-            "
-          >
-            Top Sellers
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10 gap-5">
-      <div class="">
-        <productCard
-          cat="Smart Phone"
-          name="Iphone 15 Pro Max"
-          rate="4"
-          price="400"
-        />
-      </div>
-    </div>
-  </container>
+  <TrendingProduct />
 
   <container class="mt-20">
     <div class="grid xl:grid-cols-3 gap-5">
@@ -260,23 +180,25 @@
       </div>
 
       <dealSlider class="mt-20" />
-      <BannerSlider class="mt-20"/>
+      <BannerSlider class="mt-20" />
     </container>
   </div>
 
   <container class="mt-20">
-    <ProductGrid/>
+    <ProductGrid />
   </container>
 
-  <container  class='mt-20'>
+  <container class="mt-20">
     <div class="flex flex-row items-center mb-20 justify-between">
-
       <div class="">
-        <h2 class="text-tblack font-semibold text-xl lg:text-3xl">Latest News <br> & Articles</h2>
+        <h2 class="text-tblack font-semibold text-xl lg:text-3xl">
+          Latest News <br />
+          & Articles
+        </h2>
       </div>
 
       <div class="grow hidden lg:block">
-          <hr class="w-3/4 ml-auto  bg-gray-100 h-1 mr-12" /> 
+        <hr class="w-3/4 ml-auto bg-gray-100 h-1 mr-12" />
       </div>
 
       <div class="">
@@ -286,36 +208,46 @@
           >View All Post</nuxt-link
         >
       </div>
-
     </div>
-      
 
     <blogSlider />
   </container>
 
-  <container class = 'mt-20'>
+  <container class="mt-20">
     <instagramPost />
   </container>
 
   <div class="w-full bg-torange py-12 lg:py-20 xl:py-32 mt-20">
     <container>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-2 items-center">
-  
         <div class="">
-          <span class='text-gray-200 text-base md:text-lg'>Sale %30 Of All Electronics</span>
-          <h3 class="text-2xl md:text-3xl lg:text-4xl  font-semibold text-white mt-2">Subscribe our Newsletter</h3>
+          <span class="text-gray-200 text-base md:text-lg"
+            >Sale %30 Of All Electronics</span
+          >
+          <h3
+            class="text-2xl md:text-3xl lg:text-4xl font-semibold text-white mt-2"
+          >
+            Subscribe our Newsletter
+          </h3>
         </div>
-        
+
         <div class="mt-5 md:mt-0">
-          <input type="text" v-model="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-md focus:outline-none rounded-l-md inline-block h-10 md:h-16 px-2 md:px-6 w-2/3" placeholder="johndoe@email.com" required>
-          <button class="bg-tblack text-white  h-10 md:h-16 rounded-r-md inline-block text-sm px-2 md:px-6">Subscribe</button>
+          <input
+            type="text"
+            v-model="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-md focus:outline-none rounded-l-md inline-block h-10 md:h-16 px-2 md:px-6 w-2/3"
+            placeholder="johndoe@email.com"
+            required
+          />
+          <button
+            class="bg-tblack text-white h-10 md:h-16 rounded-r-md inline-block text-sm px-2 md:px-6"
+          >
+            Subscribe
+          </button>
         </div>
-  
       </div>
     </container>
-
   </div>
-
 </template>
 
 <script setup>
@@ -323,17 +255,12 @@ import catCardCircle from "~/components/UI/catCardCircle.vue";
 import slideItem from "~/components/UI/slideItem.vue";
 import featureCard from "~/components/UI/featureCard.vue";
 import container from "~/components/UI/container.vue";
-import productCard from "~/components/UI/productCard.vue";
 import dealSlider from "~/components/dealSlider.vue";
 import { register } from "swiper/element/bundle";
-
-const activeTab = ref(1);
-const email =  ref(null);
-const setActiveTab = (tab) => {
-  activeTab.value = tab;
-};
-
 register();
+const email = ref(null);
+
+
 </script>
 
 <style scoped>
