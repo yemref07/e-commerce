c4d6e2510417508a3c85a6cc4af4a5054656819d
yemref07 <yemref0716@hotmail.com>
2023-10-30 02:20:35 +0300

    Add customCheckBox component to enhance the contact page UI. This component includes a visually appealing check effect and emits an  'update:modelValue' event for enhanced functionality.

---
+++ components/UI/customCheckbox.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="label-remember relative">
+        <input type="checkbox" :value="modelValue" @change="$emit('update:modelValue',$event.target.value)" 
+         class="hidden" />
+        <span :class="{ 'checkbox-checked checked': isChecked, checkbox: !isChecked }"></span>
+        <label class="text-sm font-medium text-gray-700 ms-6">{{label}}</label>
+        <span class="absolute left-0 right-0 top-0 bottom-0" @click="checkInput"></span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    label:String,
+    modelValue:Boolean
+})
+
+const isChecked = ref(false)
+
+const checkInput = () => {
+  isChecked.value = !isChecked.value;
+}
+</script>
+
+<style  scoped>
+.checkbox-checked{
+    position: absolute;
+    content: url(/icons/check.svg);
+    top: 4px;
+    left: 0;
+    width: 18px;
+    height: 18px;
+    line-height: 16px;
+    text-align: center;
+    visibility: visible;
+    opacity: 1;
+    color: white;
+    transition: all 0.2s 0s ease-out;
+    -webkit-transition: all 0.2s 0s ease-out;
+    -moz-transition: all 0.2s 0s ease-out;
+    -ms-transition: all 0.2s 0s ease-out;
+    -o-transition: all 0.2s 0s ease-out;
+}
+
+.checked {
+    background-color:var(--torange) ;
+    border-color: var(--torange) ;
+}
+
+.checkbox {
+    position: absolute;
+    content: "";
+    top: 4px;
+    left: 0;
+    width: 18px;
+    height: 18px;
+    line-height: 16px;
+    text-align: center;
+    border: 1px solid #C3C7C9;
+    z-index: -1;
+    -webkit-transition: all 0.2s 0s ease-out;
+    -moz-transition: all 0.2s 0s ease-out;
+    -ms-transition: all 0.2s 0s ease-out;
+    -o-transition: all 0.2s 0s ease-out;
+    transition: all 0.2s 0s ease
+}
+
+
+</style>
