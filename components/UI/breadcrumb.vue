f18b5d711a19f10811d83fdfad581798d248899f
yemref07 <yemref0716@hotmail.com>
2023-10-30 02:16:48 +0300

    - Add breadCrumb  reusable compenent  its takes title and subtitle proops

---
+++ components/UI/breadcrumb.vue
@@ -0,0 +1,28 @@
+<template>
+      <container class="mt-20">
+      <div class="flex flex-col justify-center items-center">
+        <div class="text-center">
+          <h1 class="text-tblack font-semibold text-xl md:text-2xl lg:text-4xl">
+            {{title}}
+          </h1>
+          <p class="text-muted text-lg">
+            <nuxt-link to="/" title="Home" class="hover:text-orange-500">Home</nuxt-link>
+           - {{subtitle}}
+        </p>
+        </div>
+      </div>
+    </container>
+</template>
+
+<script setup>
+import container from './container.vue';
+const props = defineProps({
+    title:String,
+    subtitle:String
+})
+
+</script>
+
+<style scoped>
+
+</style>
