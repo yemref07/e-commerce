a04cd9c2962e92f6d301fcbe29c3c600771a7af5
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-17 15:19:30 +0300

    - Add comment pinia store, This store control all of the comment related mutations and data

---
+++ store/comment.ts
@@ -0,0 +1,95 @@
+import { defineStore } from "pinia";
+
+
+interface review {
+    reviewMsg: String,
+    name: String,
+    email: String
+    rate: Number
+}
+
+export const useCommentStore = defineStore('commentStore', () => {
+    const allComments = ref<review[] | null>()
+    const newComment = ref<review>()
+    const isDeleted = ref(false)
+
+    const getAllComments = async (limit: number = 4, skip: number = 4) => {
+        try {
+            const response = await fetch(`https://dummyjson.com/comments?limit=${limit}&skip=${skip}&select=body,postId`)
+            const data = await response.json();
+            allComments.value = data.comments
+            console.log('commets data', data)
+        }
+        catch (error: any) {
+            console.error('Error Fetching Commets', error.message)
+            throw error;
+        }
+    }
+
+    const sendComment = async (comment: review) => {
+        try {
+            const response = await fetch('https://dummyjson.com/comments/add', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    body: comment.reviewMsg,
+                    postId: 3,
+                    userId: 5,
+                })
+            })
+
+            newComment.value = await response.json()
+            if (newComment.value) {
+                allComments.value?.unshift(newComment.value)
+                return true
+            }
+        }
+
+        catch (error: any) {
+            console.error(error.message, 'New comment can not sent')
+            return false
+        }
+    }
+
+    const updateComment = async (id:number,comment:string) => {
+        try {
+            const response = await fetch(`https://dummyjson.com/comments/${id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    body: comment,
+                })
+            })
+
+            const newComment = await response.json()
+            return {newComment,id}
+
+        } catch (error) {
+            console.error(error,'comments can not delete')
+        }
+    }
+
+    const deleteComment = async (id: number) => {
+        try {
+            const response = await fetch(`https://dummyjson.com/comments/${id}`, {
+                method: 'DELETE',
+            })
+            const data = await response.json()
+            isDeleted.value = data.isDeleted
+        }
+
+        catch (error: any) {
+            console.error(error.message, "Comments can not delete")
+        }
+
+    }
+
+
+    return {
+        allComments,
+        getAllComments,
+        sendComment,
+        updateComment,
+        deleteComment
+    }
+})
