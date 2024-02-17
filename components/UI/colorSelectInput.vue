d54b35e1f86f3d205857f5f6113204887c7b3282
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-17 15:15:37 +0300

    - Add color select input compenent , its expence input Color and selected Color value as a props and if it selected its show different style

---
+++ components/UI/colorSelectInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    :class="{'bg-gray-600': selectedColor === inputColor}"
+    class="w-9 h-9 rounded-full relative flex justify-center items-center border-2 cursor-pointer"
+  >
+    <div class="w-6 h-6 rounded-full absolute" :style="{backgroundColor:inputColor}"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const  props  = defineProps({
+  inputColor:{
+    type:String,
+    required:true
+  },
+  selectedColor:{
+    type:String,
+    default:false
+  }
+})
+
+
+</script>
+
