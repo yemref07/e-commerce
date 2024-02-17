6ecc4fc53e87fd1bdfba6ab64c06e522fb1005e4
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-17 15:08:52 +0300

    - Add product review card compenent it has appeling design and its expect img , name,date and comment as a prop

---
+++ components/products/product-review-card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-row justify-start items-center gap-5">
+    <img :src="props.imgSrc" alt="" width="60" class="rounded-full" />
+    <div class="p-2">
+      <Icon
+        name="solar:star-bold"
+        size="16"
+        class="text-tyellow block"
+        v-for="(item, index) in  parseInt(props.rate)"
+        :key="index"
+      />
+
+      <p class="text-sm font-semibold mt-2">
+        {{ name || 'John Doe'}}
+        <span class="text-muted text-sm">{{ date }}</span>
+      </p>
+
+      <p class="text-sm font-light text-tblack mt-2">
+        {{ comment }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  comment: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+
+  rate: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  imgSrc: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<style lang="css" scoped></style>
