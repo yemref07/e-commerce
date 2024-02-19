13dd502a2d339287800a2a00ac150f20d16ea261
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-20 02:57:50 +0300

    - Update custom checkbox compenent,  props are read only so in the compenent we cant mutate props value in two way bindings, Also  two way bindings need a event to reach input value to parent so  we cant use inputs in here in visually hidden and use for two way bindings

--- components/UI/customCheckbox.vue
+++ components/UI/customCheckbox.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="label-remember relative">
-        <input type="checkbox" :value="modelValue" @change="$emit('update:modelValue',$event.target.value)" 
-         class="hidden" />
-        <span :class="{ 'checkbox-checked checked': isChecked, checkbox: !isChecked }"></span>
-        <label class="text-sm font-medium text-gray-700 ms-6">{{label}}</label>
-        <span class="absolute left-0 right-0 top-0 bottom-0" @click="checkInput"></span>
-    </div>
+  <div class="label-remember relative">
+    <span
+      :class="{ 'checkbox-checked checked': isChecked, 'checkbox': !isChecked }"
+    ></span>
+    <label class="text-sm font-medium text-gray-700 ms-6">{{ label }}</label>
+    <span
+      class="absolute left-0 right-0 top-0 bottom-0"
+      @click="checkInput"
+    ></span>
+  </div>
 </template>
 
 <script setup>
+
 const props = defineProps({
     label:String,
     modelValue:Boolean
@@ -17,52 +21,53 @@ const props = defineProps({
 const isChecked = ref(false)
 
 const checkInput = () => {
-  isChecked.value = !isChecked.value;
+    isChecked.value = !isChecked.value
+    emit('check',isChecked.value)
 }
+
+const emit = defineEmits(['check'])
 </script>
 
-<style  scoped>
-.checkbox-checked{
-    position: absolute;
-    content: url(/icons/check.svg);
-    top: 4px;
-    left: 0;
-    width: 18px;
-    height: 18px;
-    line-height: 16px;
-    text-align: center;
-    visibility: visible;
-    opacity: 1;
-    color: white;
-    transition: all 0.2s 0s ease-out;
-    -webkit-transition: all 0.2s 0s ease-out;
-    -moz-transition: all 0.2s 0s ease-out;
-    -ms-transition: all 0.2s 0s ease-out;
-    -o-transition: all 0.2s 0s ease-out;
+<style scoped>
+.checkbox-checked {
+  position: absolute;
+  content: url(/icons/check.svg);
+  top: 4px;
+  left: 0;
+  width: 18px;
+  height: 18px;
+  line-height: 16px;
+  text-align: center;
+  visibility: visible;
+  opacity: 1;
+  color: white;
+  transition: all 0.2s 0s ease-out;
+  -webkit-transition: all 0.2s 0s ease-out;
+  -moz-transition: all 0.2s 0s ease-out;
+  -ms-transition: all 0.2s 0s ease-out;
+  -o-transition: all 0.2s 0s ease-out;
 }
 
 .checked {
-    background-color:var(--torange) ;
-    border-color: var(--torange) ;
+  background-color: var(--torange);
+  border-color: var(--torange);
 }
 
 .checkbox {
-    position: absolute;
-    content: "";
-    top: 4px;
-    left: 0;
-    width: 18px;
-    height: 18px;
-    line-height: 16px;
-    text-align: center;
-    border: 1px solid #C3C7C9;
-    z-index: -1;
-    -webkit-transition: all 0.2s 0s ease-out;
-    -moz-transition: all 0.2s 0s ease-out;
-    -ms-transition: all 0.2s 0s ease-out;
-    -o-transition: all 0.2s 0s ease-out;
-    transition: all 0.2s 0s ease
+  position: absolute;
+  content: "";
+  top: 4px;
+  left: 0;
+  width: 18px;
+  height: 18px;
+  line-height: 16px;
+  text-align: center;
+  border: 1px solid #c3c7c9;
+  z-index: -1;
+  -webkit-transition: all 0.2s 0s ease-out;
+  -moz-transition: all 0.2s 0s ease-out;
+  -ms-transition: all 0.2s 0s ease-out;
+  -o-transition: all 0.2s 0s ease-out;
+  transition: all 0.2s 0s ease;
 }
-
-
 </style>
