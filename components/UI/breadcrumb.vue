2bd789fef0696d87ac2806930473c26574ef7dfb
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-20 03:00:03 +0300

    -Update bread crumb compenent, now its more customazible , now can also decide alignment of the compenent like middle of the page left-start

--- components/UI/breadcrumb.vue
+++ components/UI/breadcrumb.vue
@@ -1,28 +1,46 @@
 <template>
-      <container class="mt-20">
-      <div class="flex flex-col justify-center items-center">
-        <div class="text-center">
-          <h1 class="text-tblack font-semibold text-xl md:text-2xl lg:text-4xl">
-            {{title}}
-          </h1>
-          <p class="text-muted text-lg">
-            <nuxt-link to="/" title="Home" class="hover:text-orange-500">Home</nuxt-link>
-           - {{subtitle}}
+  <container class="mt-20">
+    <div
+      class="flex flex-col justify-center align-baseline"
+      :class="{
+        'items-center': alignment === 'center',
+        'items-baseline': alignment === 'baseline',
+      }"
+    >
+      <div :class="{'text-start': alignment === 'baseline','text-center': alignment === 'center'}">
+        <h1 class="text-tblack font-semibold text-xl md:text-2xl lg:text-4xl">
+          {{ title }}
+        </h1>
+        <p class="text-muted text-lg">
+          <nuxt-link to="/" title="Home" class="hover:text-orange-500"
+            >Home</nuxt-link
+          >
+          - {{ subtitle }}
         </p>
-        </div>
       </div>
-    </container>
+    </div>
+  </container>
 </template>
 
 <script setup>
-import container from '~/components/UI/container.vue';
+import container from "~/components/UI/container.vue";
 const props = defineProps({
-    title:String,
-    subtitle:String
-})
-
+  title: {
+    type: String,
+    default: "Page Title",
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    default: "Page Subtitle",
+    required: true,
+  },
+  alignment: {
+    type: String,
+    default: "baseline",
+    required: true,
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
