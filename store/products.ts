67cb4a4232dc53acda7c8e338d2b3a67795772fc
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-23 02:58:58 +0300

    Ad new functions to Pinia store for managing product data fetching
    - `searchProducts`: Searches for products by type and updates the search results.
    
    - `getProductsOfCategory`: Fetches all products of a given category and updates the categoryProducts state.

--- store/products.ts
+++ store/products.ts
@@ -1,42 +1,164 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useProductsStore = defineStore ('productsStore', () =>{ 
+interface productData {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
+
+export const useProductsStore = defineStore("productsStore", () => {
     //All products categories
-    const allProductsCat = ref< string []>([])
+    const allProductsCat = ref<string[]>([]);
+    const singleProduct = ref<productData>();
+    const searchResult = ref<productData[]>();
+    const loadStatus = ref<boolean>(false);
+    const categoryProducts = ref<productData[]>();
 
     //fetch all product categories
     const getAllProductsCat = async () => {
+        loadStatus.value = true;
         try {
-            const response = await fetch('https://dummyjson.com/products/categories')
-            const data = await response.json()
-            console.log(response)
+            const response = await fetch("https://dummyjson.com/products/categories");
+            const data = await response.json();
+            console.log(response);
 
             //check reponse status because non-200 responses won't throw JavaScript errors by default
-            if(!response.ok){
-                throw new Error('Failed to fetch products categiroes',response.status)
+            if (!response.ok) {
+                throw new Error("Failed to fetch products categiroes");
+                return false;
             }
 
             //check reponse data is an array
             if (Array.isArray(data)) {
-                allProductsCat.value = [...data]
+                allProductsCat.value = [...data];
             }
 
             //if not throw error
-            else{
-                throw new Error('İnvalid data formate received')
+            else {
+                throw new Error("İnvalid data formate received");
             }
 
-            return true
+            return true;
+        } catch (error) {
+            console.error(error, "all products can not fetch");
+            return false;
+        }
+
+        finally{
+            loadStatus.value = false;
+        }
+    };
+
+    //Fetch data of single product with using ID
+    const getSingleProduct = async (productID: number) => {
+        loadStatus.value = true;
+        try {
+            const response = await fetch(
+                `https://dummyjson.com/product/${productID}`
+            );
+            console.log(response);
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch signle products");
+                return false;
+            }
+
+            //ensure the fetched response in correct data format
+            if (typeof data === "object") {
+                singleProduct.value = data;
+                return true;
+            } 
+            
+            else {
+                throw new Error("Recieved product data format is not valid");
+            }
         } 
         
         catch (error) {
-            console.error(error, 'all products can not fetch')
-            return false
+            console.error(error, "Cant fetch to product data");
+            return false;
         }
+
+        finally{
+            loadStatus.value = false;
+        }
+    };
+
+    // It takes product types as a param like phone, notebook ....
+    const searchProducts = async (type: string) => {
+        loadStatus.value = true;
+        try {
+            const response = await fetch(
+                `https://dummyjson.com/products/search?q=${type}`
+            );
+           
+            if (!response.ok) {
+                throw new Error("Failed search product by type of the product");
+            }
+
+            const data = await response.json();
+
+            if (data) {
+                searchResult.value = data?.products;
+                return true;
+            } 
+            
+            else {
+                searchResult.value = [];
+            }
+        } 
+        
+        catch (error) {
+            console.error(error, "Failed to fect products with type ");
+        } 
+        
+        finally {
+            loadStatus.value = false;
+        }
+    };
+
+    //  Function to fetch all products of a given category
+    const getProductsOfCategory = async (catName:string) => {
+        loadStatus.value =  true;
+        try {
+            const response = await fetch(`https://dummyjson.com/products/category/${catName}`)
+
+            if(!response.ok){
+                throw new Error(`Failed to fetch products for category ${catName}. Status: ${response.status}`);
+                return false;
+            }
+    
+            const data = await response.json();
+            categoryProducts.value = data?.products;
+        } 
+        
+        catch (error) {
+            console.error("Failed fetch categorys products",error)
+            return false;
+        }
+
+        finally{
+            loadStatus.value = false;
+        }
+
+
     }
 
-    return{
+
+    return {
         allProductsCat,
-        getAllProductsCat
-    }
-})
+        getAllProductsCat,
+        getSingleProduct,
+        searchProducts,
+        getProductsOfCategory
+    };
+});
