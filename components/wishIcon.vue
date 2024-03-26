4949818fe6492cdcc09469520e5dfcd0c3a31879
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-26 04:01:03 +0300

    - Implements a visually appealing wish icon component. - Accepts a `quantity` prop to dynamically display the number of products in the wishlist. - Enhances user experience by providing a clear visual representation of wishlist items.

---
+++ components/wishIcon.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col relative cursor-pointer" @click="goWishlistPage">
+      <Icon name="mdi:heart" size="26" class="block mr-5  relative text-tblack " />
+      <span
+        class=" text-white text-xs font-semibold px-2 py-1 rounded-full absolute badge"
+        >{{props.quantity || 0 }}</span
+      >
+    </div>
+  </template>
+  
+  <script setup>
+const props = defineProps({
+  quantity:{
+    type:Number,
+    required:true,
+    default:1
+  }
+})
+
+const router = useRouter()
+
+  const goWishlistPage = () => {
+    router.push({path:"/wishlist"})
+  }
+
+  </script>
+  
+  <style scoped>
+  .badge{
+      left: 40%;
+      top: -15%;
+      background-color: var(--torange);
+  }
+  </style>
+  
