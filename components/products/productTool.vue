e70cff425aa19f90653a4a8544cd9c2e08ec6e87
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-26 04:27:08 +0300

    Here's an improved commit message for enhancing the product tool functionality:
    feat: Implement functionality for add to cart and wishlist buttons in productTool
    
    Transformed previously non-functional "Add to Cart" and "Wishlist" buttons in the productTool component into interactive elements.
    Enabled users to add products to their cart or wishlist directly from the product tool.
    Improved user experience by providing convenient product interaction options.

--- components/products/productTool.vue
+++ components/products/productTool.vue
@@ -2,11 +2,12 @@
     <div class="flex flex-col border toolbox rounded-md  items-center  justify-center" :class="{'toolbox-hover': hover}">
 
         <div class="tool border-b-2 p-2">
-            <Icon name="clarity:shopping-cart-line" class="" size="28" />
+            <Icon name="clarity:shopping-cart-line" class="" size="28" @click="addProductToCart" />
         </div>
 
         <div class="tool border-b-2 p-2">
-            <Icon name="ph:heart" class="" size="28" />
+            <Icon name="ph:heart" size="28" v-if="!wishStatus" @click="addProductToWishList"/>
+            <Icon name="ph:heart-fill" size="28" v-else color="red" @click="removeFromWishlist"/>
         </div>
         
         <nuxt-link :to="`/categories/${category}/${name}`">
@@ -14,19 +15,82 @@
                 <Icon name="solar:eye-broken" class="" size="28" />
             </div>
         </nuxt-link>
-
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCartStore } from '~/store/cart';
+import { useWishlistStore } from '~/store/wishlist';
+
 const props = defineProps({
     hover:Boolean,
-    link:String,
-    productID:String,
+    productID:Number,
     category:String,
-    name:String
-
+    name:String,
+    image:String,
+    price:Number
 })
+
+//Cart Store
+const cartStore = useCartStore()
+const { addCart } = cartStore
+const { cartList} =  storeToRefs(cartStore)
+
+const addProductToCart = () => {
+    if(props.category && props.productID && props.name && props.image && props.price){
+        addCart({
+            name:props.name,
+            id:props.productID ,
+            category:props.category,
+            quantity:1,
+            image:props.image,
+            price:parseFloat(props.price)
+        })
+    }
+    else{
+        console.error("This product does not have an essential data for adding to cart")
+    }
+}
+
+
+//wishlist store
+
+const wishlistStore = useWishlistStore()
+const { addWishList, removeWishList} = wishlistStore
+const { wishlist} = storeToRefs(wishlistStore)
+
+const removeFromWishlist = () => {
+    if(props.productID){
+        removeWishList(props.productID)
+    }
+    else{
+        console.error("This product does not have an proper ID")
+    }
+}
+
+const addProductToWishList = () => {
+    if(props.category && props.productID && props.name && props.image && props.price){
+        addWishList({
+            name:props.name,
+            id:props.productID ,
+            category:props.category,
+            quantity:1,
+            image:props.image,
+            price:props.price
+        })
+    }
+    else{
+        console.error("This product does not have an essential data for adding to wishlist")
+    }
+}
+
+const wishStatus = computed(()=>{
+    return wishlist.value.some((item)=>{
+      return  item.id === props.productID
+    })
+})
+
 
 </script>
 
