949c5e314ff25367822eddf9dbad3d30ea35de37
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-26 04:33:06 +0300

    feat:Enhance , link of the component become more seo and users friendly.

--- components/UI/catCardCircle.vue
+++ components/UI/catCardCircle.vue
@@ -2,13 +2,13 @@
     <div class="flex flex-col justify-center items-center gap-5">
 
         <div class="flex flex-col justify-center items-center cat-circle rounded-full">
-            <nuxt-link :to="link" class="cursor-pointer">
-                <img :src="img" alt="Product Category 1" width="78">
+            <nuxt-link :to="link" class="cursor-pointer" :title="props.title">
+                <img :src="img" alt="Product Category 1" width="78" :alt="props.title">
             </nuxt-link>
         </div>
 
         <div class="text-center">
-            <nuxt-link :to="link" class="cursor-pointer">
+            <nuxt-link :to="link" class="cursor-pointer" :title="props.title">
                 <h3 class="font-semibold text-xl">{{ title }}</h3>
                 <p>{{ productStock }} Products</p>
             </nuxt-link>
