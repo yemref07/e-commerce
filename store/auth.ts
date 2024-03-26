461aa5f5be77f7e4a6fe3b1bb67c121cd1b8169a
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-27 02:19:44 +0300

    feat: Implement improved authentication with semantic error messages, token storage, and logout functionality
    Enhanced error handling: Provides informative error messages to users based on invalid credentials (semantic error messages).
    Secure token storage: Stores authentication tokens securely in browser cookies.
    Persistent authentication: Enables user authentication throughout the application using stored tokens.
    Logout functionality: Implements a logout feature to remove tokens and clear user session.

--- store/auth.ts
+++ store/auth.ts
@@ -1,11 +1,28 @@
 import { defineStore } from "pinia";
 
 interface login {
-    userName: string,
+    username: string,
     password: string
 }
 
+type Gender = "male" | "female" | "other";
+
+type user = {
+    readonly id:number,
+    email: string,
+    firstName: string,
+    lastName: string,
+    gender?: Gender,
+    image?: string,
+    token:string,
+}
+
 export const useAuthStore = defineStore('authStore', () => {
+    const isAuthenticated = ref(false)
+    const token = useCookie('token', { maxAge: 60000, domain: "localhost", sameSite: 'lax' })
+    const errorMsg = ref("")
+    const userData = ref<user>();
+
     const signIn = async (param: login) => {
         try {
 
@@ -14,36 +31,56 @@ export const useAuthStore = defineStore('authStore', () => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(
                     {
-                        username: param.userName,
+                        username: param.username,
                         password: param.password
                     }
                 )
             });
 
-            if(!response.ok){
-                throw new Error(`Failed Sign In : status: ${response.status}`);
-                return false;              
-            }
-
             const data = await response.json()
-            if(data.token){
-                const token = useCookie('token')
+
+            //In some cases event can response status can be ok but we can get an error like invalid credentials with status code:400
+            if (!response.ok) {
+                console.log(data)
+                errorMsg.value = data.message;
+                return false;
+            }
+
+            if (data?.token) {
                 token.value = data.token;
+                isAuthenticated.value = true;
+                userData.value = data;
+                console.log('userData:',userData.value)
+                return true;
+            }
+
+            else {
+                throw new Error(`Token in response is null or undefined`);
             }
 
 
-        } 
-        
-        catch (error) {
+        }
 
+        catch (error: any) {
+            console.error(`Something wrong about login ${error}`)
+            return false;
         }
 
 
 
     }
 
+    const logout = () => {
+        token.value = null;
+        isAuthenticated.value = false;
+    }
+
 
     return {
-        signIn
+        signIn,
+        logout,
+        isAuthenticated,
+        userData,
+        errorMsg
     }
 })
