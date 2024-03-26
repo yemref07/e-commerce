a087dcaba1c3f0265516169f93f254a1632f9ea7
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-26 04:29:04 +0300

    feat: Improve accessibility of blogSlideItems with dynamic alt tags
    - Enhanced accessibility of the `blogSlideItems` component by implementing dynamic alt tags.
    - Alt tags will now be generated based on relevant information for each slide item.
    - This improves user experience for screen reader users and SEO.

--- components/sliders/blogSlideItem.vue
+++ components/sliders/blogSlideItem.vue
@@ -2,13 +2,13 @@
   <div class="flex flex-col gap-5 justify-center">
     <div class="relative">
         <nuxt-link to="/">
-            <img :src="img" alt="" class="w-full h-auto rounded-md" />
+            <img :src="img" :alt="props.title" class="w-full h-auto rounded-md" />
         </nuxt-link>
         <span class="bg-tblack text-white px-6 py-2 absolute left-0 top-0">10 Sep 2023</span>
     </div>
 
     <div class="">
-      <nuxt-link to="/">
+      <nuxt-link to="/" :alt="props.title">
         <h3 class="text-tblack text-xl lg:text-xl xl:text-2xl mb-2 font-semibold hover:text-orange-600 transition-all delay-50">
           {{ title }}
         </h3>
@@ -20,7 +20,7 @@
       <p class="mt-3">{{ shortDecs }}...</p>
       <nuxt-link
         to="/"
-        class="bg-white border-2 mt-3 rounded-md inline-block border-slate-200 px-6 py-2 hover:bg-orange-500 hover:text-white transition-all delay-75 hover:border-orange-500"
+        class="bg-white border-2 mt-3 rounded-md inline-block border-slate-200 px-6 py-2 hover:bg-orange-500  hover:text-white transition-all delay-75 hover:border-orange-500"
       >
         Read More
         <Icon name="system-uicons:arrow-right" class="" size="24" />
