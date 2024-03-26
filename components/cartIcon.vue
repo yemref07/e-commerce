c247b9d757edeaa157ffe31db5526b92cd2c5801
yunus emre fersiz <yemref0716@hotmail.com>
2024-03-26 04:00:19 +0300

    - Implements a visually appealing cart icon component. - Accepts a `productCount` prop to dynamically display the number of products in the cart. - Enhances user experience by providing a clear visual representation of cart items.

---
+++ components/cartIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col relative cursor-pointer">
+    <Icon name="gala:bag" size="26" class="block mr-5 text-tblack relative" />
+    <span
+      class=" text-white text-xs font-semibold px-2 py-1 rounded-full absolute badge"
+      >{{props.quantity || 0 }}</span
+    >
+  </div>
+</template>
+
+<script setup>
+const router = useRouter()
+
+const props = defineProps({
+  quantity:{
+    type:Number,
+    required:true,
+    default:1
+  }
+})
+</script>
+
+<style scoped>
+.badge{
+    left: 40%;
+    top: -15%;
+    background-color: var(--torange);
+}
+</style>
