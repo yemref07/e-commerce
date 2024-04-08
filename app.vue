b93cb71072e13d445e340454880ad9008d403a3b
yunus emre fersiz <yemref0716@hotmail.com>
2024-04-08 03:48:04 +0300

    feat: Implement automatic user data fetching on login persistence

--- app.vue
+++ app.vue
@@ -6,6 +6,17 @@
 
 
 <script setup>
+import { useAuthStore } from './store/auth';
+const authStore = useAuthStore()
+const token = useCookie('token')
+const { getAuthUser } = authStore
+const {userData} = storeToRefs(authStore)
 
+onMounted(async ()=>{
+  if(token.value && !userData.value){
+    await getAuthUser()
+  }
+  
+})
 
 </script>
