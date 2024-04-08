de2b5b1cab4b4bd227b6323ece9ac20f7ea84de7
yunus emre fersiz <yemref0716@hotmail.com>
2024-04-08 03:53:15 +0300

    feat:Remove checking token in here it only work for route change but we need this logic for every time when the app is open

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,18 +1,16 @@
 import { storeToRefs } from "pinia"
 import { useAuthStore } from "~/store/auth"
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware ( async (to, from) => {
   const authStore = useAuthStore()
   const token = useCookie('token')
-  const {isAuthenticated} =  storeToRefs(authStore)
-
-  if(token?.value){
-    isAuthenticated.value = true
-  }
+  const {isAuthenticated,userData} =  storeToRefs(authStore)
+  const {getAuthUser,logout} = authStore;
 
   // Nuxt 3 middleware functions are designed to return a value to determine how the middleware chain proceeds.
   // Returning null or undefined indicates continuing the middleware execution as usual.
   // Returning a value with redirection logic (like navigateTo) tells Nuxt 3 to immediately interrupt the middleware chain and perform the redirection.
   // so because of the in below we returning the navigateTo function
+
 
   //if token is exist and users in sign-in page 
   if (isAuthenticated && to?.name === 'signin') {
