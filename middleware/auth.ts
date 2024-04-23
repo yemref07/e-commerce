4c613884847e53425d97eeb63de9d3b6ccfdf0d1
yunus emre fersiz <yemref0716@hotmail.com>
2024-04-24 01:47:20 +0300

    Add token control in condition for some cases just checking isAuth state could be not enough

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -10,15 +10,15 @@ export default defineNuxtRouteMiddleware ( async (to, from) => {
   // Returning null or undefined indicates continuing the middleware execution as usual.
   // Returning a value with redirection logic (like navigateTo) tells Nuxt 3 to immediately interrupt the middleware chain and perform the redirection.
   // so because of the in below we returning the navigateTo function
-
+  
 
   //if token is exist and users in sign-in page 
-  if (isAuthenticated && to?.name === 'signin') {
+  if (isAuthenticated && token.value && to?.name === 'signin') {
     return navigateTo('/');
   }
 
   //if token not exist and user try to enter user-profile page redirect it to home
-  if( !isAuthenticated && to?.name === "user-profile"){
+  if( !isAuthenticated && !token.value && to?.name === "user-profile"){
     return navigateTo('/')
   }
   
