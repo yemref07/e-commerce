ca519631fa0470cd92a0323b35e3cac06ab8830e
yunus emre fersiz <yemref0716@hotmail.com>
2024-04-24 03:51:59 +0300

    Set html lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-    }
+    },
+    htmlAttrs: {
+      lang: 'en'
+    },
   },
   components: {
     global: true,
@@ -80,7 +83,7 @@ export default defineNuxtConfig({
         }
       }
     },
-    provider: 'netlify',
+    // provider: 'netlify',
   },
 
   piniaPersistedstate: {
